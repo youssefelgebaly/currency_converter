d9c6e5ff2516c20e776f64b3873fc1b7c1d18ca7
youssefelgebaly <you@example.com>
2022-07-27 01:59:06 +0200

    id text input layout

--- app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.currencyconverterapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val convertBtn :Button = findViewById(R.id.convert_button)
+        val amountEt:TextInputEditText = findViewById(R.id.amount_edit_text)
+        val resultEt:TextInputEditText = findViewById(R.id.result_edit_text)
+
+        convertBtn.setOnClickListener{
+            val amount =amountEt.text.toString().toDouble()
+            val result=amount.times(19.85)
+
+            resultEt.setText(result.toString())
+        }
+
     }
 }
